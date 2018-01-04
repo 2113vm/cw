9386c2625ed7b7c65f13aea751b41cd8b7d56995
googolplex <21138532110q@gmail.com>
2018-01-05 02:10:00 +0500

    create loginForm

--- CW/Form1.Designer.cs
+++ CW/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CW
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPass = new System.Windows.Forms.Label();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(37, 30);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(41, 13);
+            this.labelLogin.TabIndex = 0;
+            this.labelLogin.Text = "Логин:";
+            // 
+            // labelPass
+            // 
+            this.labelPass.AutoSize = true;
+            this.labelPass.Location = new System.Drawing.Point(37, 102);
+            this.labelPass.Name = "labelPass";
+            this.labelPass.Size = new System.Drawing.Size(48, 13);
+            this.labelPass.TabIndex = 1;
+            this.labelPass.Text = "Пароль:";
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(40, 63);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(146, 20);
+            this.textBoxLogin.TabIndex = 2;
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Location = new System.Drawing.Point(39, 132);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.Size = new System.Drawing.Size(147, 20);
+            this.textBoxPass.TabIndex = 3;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(75, 176);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Вход";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(238, 229);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.labelPass);
+            this.Controls.Add(this.labelLogin);
+            this.Name = "LoginForm";
+            this.Text = "Вход";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPass;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.Button buttonLogin;
     }
 }
 
