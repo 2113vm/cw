cf770ff93ee5e69d4813117ece17bd74aebd6d0a
googolplex <21138532110q@gmail.com>
2018-01-05 02:05:51 +0500

    add current user in application context

--- CW/ApplicationContext.cs
+++ CW/ApplicationContext.cs
@@ -12,6 +12,7 @@ namespace CW
         {
         }
 
+        public User CurrentUser { get; set; }
         public virtual DbSet<Contract> Contracts { get; set; }
         public virtual DbSet<Payment> Payments { get; set; }
         public virtual DbSet<Role> Roles { get; set; }
